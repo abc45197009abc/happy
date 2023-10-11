66ca586752cf10be559cc1b434c498e1c0b96994
abc45197009abc <11056032@ntub.edu.tw>
2023-10-11 16:19:14 +0800

    purplesoftware

--- mysite/views.py
+++ mysite/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from mysite.models import Post
+from django.http import HttpResponse
+from datetime import datetime
+def homepage(request):
+    posts = Post.objects.all()
+    now = datetime.now()
+    return render(request, 'index.html', locals())
+def showpost(request, slug):
+    post = Post.objects.get(slug = slug) 
+    #select* from post where slug = %slug
+    return render(request, 'post.html', locals())
 
-# Create your views here.
+
+
+'''
+def homepage(request):
+    posts = Post.objects.all()
+    post_lists = list()
+    for count,post in enumerate(posts):
+        post_lists.append(f'No. {count} - {post} <br>')
+        return HttpResponse(post_lists)
+'''
