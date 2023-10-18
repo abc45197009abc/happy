84be9422e57b0c431ca4d6c040c6348d0f1ca75b
abc45197009abc <11056032@ntub.edu.tw>
2023-10-18 15:22:31 +0800

    yuzusoft

--- happy/urls.py
+++ happy/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for happy project.
+URL configuration for mblog0927 project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -17,8 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from mysite import views as mv
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',mv.homepage,name="homepage"),
-    path('post/<slug:slug>/', mv.showpost,),
+    path('', mv.homepage, name="homepage"),
+    path('post/<slug:slug>/', mv.showpost, name="showpost"),
 ]
